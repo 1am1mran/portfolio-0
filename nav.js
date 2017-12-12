1221a4cb0110f96bad751f71a32f9627fb3acf46
1am1mran <imran.abdul.186@gmail.com>
2017-12-12 12:27:57 +0530

    files modified

---
+++ nav.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("window").load(function(){
+        $("nav").animate({
+            right: '300px'
+        });
+    });
+    // $("#show").click(function(){
+    //     $("p").show();
+    // });
+});
