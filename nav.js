2dcb095e3c63f25a4525e8c6d0bea132864bb0a6
1am1mran <imran.abdul.186@gmail.com>
2017-12-21 23:05:15 +0530

    modified files

--- nav.js
+++ nav.js
@@ -1,10 +1,23 @@
-$(document).ready(function(){
-    $("window").load(function(){
-        $("nav").animate({
-            right: '300px'
+$(function () {
+    $('nav ul').hide();
+    // $('#first-block h1').hide();
+});
+
+setInterval(function () {
+    $('nav ul').show().addClass("animated fadeIn");
+}, 1500);
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 15) {
+        $('nav').css({
+            'background-color': 'rgb(77, 77, 77)'
         });
-    });
-    // $("#show").click(function(){
-    //     $("p").show();
-    // });
+    }
+    else {
+        $('nav').css({
+            'background-color': 'rgb(32, 27, 27)'
+        });
+    }
+
 });
