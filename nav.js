84011fc70d6a2087caf6fb1feba991d7842d3c8f
1am1mran <imran.abdul.186@gmail.com>
2017-12-25 13:15:33 +0530

    files modified

--- nav.js
+++ nav.js
@@ -1,23 +1,30 @@
-$(function () {
-    $('nav ul').hide();
-    // $('#first-block h1').hide();
-});
 
-setInterval(function () {
-    $('nav ul').show().addClass("animated fadeIn");
-}, 1500);
+$(document).ready(function () {
+    $(function () {
+        $('nav ul').hide();
+        // $('#first-block h1').hide();
+    });
+
+    setInterval(function () {
+        $('nav ul').show().addClass("animated fadeIn");
+    }, 1500);
 
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 15) {
-        $('nav').css({
-            'background-color': 'rgb(77, 77, 77)'
-        });
-    }
-    else {
-        $('nav').css({
-            'background-color': 'rgb(32, 27, 27)'
-        });
-    }
+    $(window).scroll(function () {
+        // let navbar = $("nav ul");
+        // navbar.toggle({"left" : "20px"},slide, 1000);
+        if ($(window).scrollTop() > 15) {
+            // $(navbar).animate({left: '350px'});
+            $('nav').css({
+                'background-color': 'rgb(77, 77, 77)'
+            });
+        }
+        else {
+            // $(navbar).animate({left: '350px'});
+            $('nav').css({
+                'background-color': 'rgb(32, 27, 27)'
+            });
+        }
 
+    });
 });
